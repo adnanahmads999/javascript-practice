e978cbee9bb6506f486def4d1b27feedea087cf7
adnanahmadsxd <adnanahmads999@gmail.com>
2023-11-27 17:09:42 +0530

    var, let, const practice

---
+++ variables.js
@@ -0,0 +1,15 @@
+const name= "windows"
+var id= "windows@gmail.com"
+let password= "54321"
+accCity= "Lucknow"
+
+//const can not be changed
+
+id= "network@gmail.com"
+password="7896"
+accCity= "Mumbai"
+
+console.table([name, id, password, accCity]); //to display data in tabular form
+
+// Prefer not to use var
+// because of issue in block scope and functional scope
